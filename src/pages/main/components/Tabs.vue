8a5447f36e555f0d967ccd0e7a4f84a20c0972c1
Dmitriy Dultsev <tsukikohaya@gmail.com>
2023-10-12 14:02:34 +0300

    create upload file tab

--- src/pages/main/components/Tabs.vue
+++ src/pages/main/components/Tabs.vue
@@ -1,16 +1,53 @@
 <script setup lang="ts">
 import MyInput from "../../../components/ui/MyInput.vue";
+
+function submitHandler(e: Event) {
+   e.preventDefault();
+   const form = e.currentTarget as HTMLFormElement;
+   const formdata = new FormData(form);
+   const target = e.target as HTMLInputElement;
+   const files = target.files;
+   if (files) {
+      formdata.append("files", files[0], encodeURI(files[0].name));
+      const fetchOptions: RequestInit = {
+         method: "POST",
+         body: formdata,
+         headers: {
+            authorization:
+               "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Inp4Y0BtYWlsLnJ1IiwiaWF0IjoxNjk3MTA1NjM0LCJleHAiOjE2OTcxOTIwMzR9.XC0n959HdY5jYKdDo6JTrnE-Ief61C3fKrnz24aQix8",
+         },
+      };
+      fetch("http://localhost:3333/files", fetchOptions)
+         .then(data => console.log(data))
+         .catch(e => console.log(e));
+   }
+}
 </script>
 
 <template>
-   <div class="tab__menu flex w-96 justify-between">
-      <MyInput class="w-1/2 rounded-md shadow-md" :type="'text'" :placeholder="'find'"/>
+   <div class="tab__menu flex w-[500px] justify-between">
+      <MyInput
+         class="w-1/2 rounded-md shadow-md"
+         :type="'text'"
+         :placeholder="'find'" />
+      <form class="ml-2" @change="submitHandler" enctype="multipart/form-data">
+         <label for="fileinput" class="block h-full cursor-pointer">
+            <div
+               class="border-grey flex h-full select-none items-center rounded-md shadow-md transition-colors active:bg-emerald-300">
+               <span class="px-4">Add files</span>
+               <input
+                  multiple
+                  type="file"
+                  id="fileinput"
+                  class="hidden"
+                  accept="image/*, text/*, .json"
+                   />
+            </div>
+         </label>
+      </form>
+
       <button
-         class="mx-2 rounded-md bg-emerald-300 px-4 transition-colors  active:bg-emerald-400 shadow-md">
-         Add
-      </button>
-      <button
-         class="mx-2 rounded-md bg-emerald-300 px-4 transition-colors  active:bg-emerald-400 shadow-md">
+         class="mx-2 rounded-md bg-emerald-300 px-4 shadow-md transition-colors active:bg-emerald-400">
          Remove
       </button>
    </div>
